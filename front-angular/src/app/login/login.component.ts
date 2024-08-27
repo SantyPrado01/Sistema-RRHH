b9633546baf6d65bc8f4e4faae887b06b35c39d1
SantyPrado01 <santiago01prado@gmail.com>
2024-08-26 23:39:35 -0300

    Comienzos del FrontEnd

---
+++ front-angular/src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  passwordControl = new FormControl('');
+  isPasswordVisible = false;
+
+  togglePasswordVisibility(): void{
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+}
