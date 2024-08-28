162069579e8b0a91b1f8d780278ad0e3f442d27d
Santy <santiago01prado@gmail.com>
2024-08-28 12:10:39 -0300

    Comienzo de LOGIN

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:4200/',
+    methods: 'GET,POST,PUT,DELETE, PATCH',
+    allowedHeaders: 'Content-Type,Authorization',   
+});
 }
 bootstrap();
