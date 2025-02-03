2a1e3cfa0a392f6e3430eb7e01bbffb82bab7610
Santy <santiago01prado@gmail.com>
2025-02-03 17:31:31 -0300

    Cambios

--- back-nest/src/main.ts
+++ back-nest/src/main.ts
@@ -1,19 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { AuthGuard } from './auth/guard/auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
   app.enableCors({
-    origin: 'http://localhost:4200',  // Cambia esto según el origen de tu frontend
+    origin: 'http://localhost:4200',  // Cambiar Frontendn
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true
   });
 
-  // Pipes de validación global
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
